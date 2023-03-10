0b0a8fd237836746a896c5822f84c09e25449f3a
Aditya <rajaditya3759@gmail.com>
2023-03-10 12:21:40 +0530

    error message fixed in AppProvider

--- src/Global Context/AppProvider.js
+++ src/Global Context/AppProvider.js
@@ -1,6 +1,8 @@
 import React, { createContext, useState } from "react";
 import Web3 from "web3";
 import loadContract from "../contracts/loadContract";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 const AppContext = createContext();
 
 const AppProvider = ({ children }) => {
@@ -11,22 +13,31 @@ const AppProvider = ({ children }) => {
   });
   const connectWalletHandler = async () => {
     if (typeof window.ethereum !== "undefined") {
-      const accounts = await window.ethereum.request({
-        method: "eth_requestAccounts",
-      });
-      const web3 = new Web3(window.ethereum);
-      const contract = await loadContract(web3);
-      setWeb3Api({
-        web3,
-        accounts: accounts[0],
-        contract,
-      });
+      try {
+        const accounts = await window.ethereum.request({
+          method: "eth_requestAccounts",
+        });
+        const web3 = new Web3(window.ethereum);
+        const contract = await loadContract(web3);
+        setWeb3Api({
+          web3,
+          accounts: accounts[0],
+          contract,
+        });
+      } catch (error) {
+        toast.error("Request rejected!!");
+      }
+    } else {
+      toast.warn("Please install MetaMask");
     }
   };
   return (
-    <AppContext.Provider value={{ connectWalletHandler, ...web3Api }}>
-      {children}
-    </AppContext.Provider>
+    <>
+      <AppContext.Provider value={{ connectWalletHandler, ...web3Api }}>
+        {children}
+      </AppContext.Provider>
+      <ToastContainer />
+    </>
   );
 };
 
